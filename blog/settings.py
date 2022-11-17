23cda1026789763c68e8020a75aafa46d33ab0be
vizvasrj <vizvasrj@gmail.com>
2022-11-17 22:50:52 +0530

    git

--- blog/settings.py
+++ blog/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-3psn2&2wu$ypk)fe97e4n#3_!@nwh-aw)=b#or9g&62k#(gn%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://demo-blog-dx0g.onrender.com/",
+    "http://demo-blog-dx0g.onrender.com/"
+    ]
 
 # Application definition
 
@@ -83,13 +87,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'], engine='django_cockroachdb')}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': env("DB_ENGINE"),
